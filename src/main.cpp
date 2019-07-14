a80b48bcd9bf736688a695c609ac92dcfb65c115
Matthew D Jones <matthew.d.jones@tessella.com>
2019-07-14 13:34:33 +0100

    Add all compiler warning for gcc and clang, level 4 for msvc

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,5 @@
-#include <iostream>
-
-/// Square a double.
-///
-/// Typical usage:
-/// \code
-///   square(3.8);
-/// \endcode
-///
-/// \param value Double value to be squared.
-///
-/// \returns The square of `value`.
-double square(double value) {
-  return value * value;
-}
+#include <fmt/format.h>
 
 int main(int argc, char **argv) {
-  std::cout << "Hello World" << std::endl;
+  fmt::print("Hello World");
 }
