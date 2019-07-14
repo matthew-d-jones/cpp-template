bf3f88062b20a0d38c487fc02c8e2c235d36f1cd
Matthew D Jones <matthew.d.jones@tessella.com>
2019-07-14 13:10:38 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/// Square a double.
+///
+/// Typical usage:
+/// \code
+///   square(3.8);
+/// \endcode
+///
+/// \param value Double value to be squared.
+///
+/// \returns The square of `value`.
+double square(double value) {
+  return value * value;
+}
+
+int main(int argc, char **argv) {
+  std::cout << "Hello World" << std::endl;
+}
